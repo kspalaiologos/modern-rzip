f722e48fa840b5b091dd408dfd9b215e1d81fbf1
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-10-13 23:23:54 +0200

    man fuck macos such a pain in the ass

--- ar-mrzip/ar-mrzip.cpp
+++ ar-mrzip/ar-mrzip.cpp
@@ -55,12 +55,11 @@ class blake2b_cksum {
 class file {
     public:
         /*  0 */ uint64_t modification_date;
-        /*  8 */ uint64_t creation_date;
-        /* 16 */ uint64_t size;
-        /* 24 */ uint64_t archive_offset;
-        /* 32 */ blake2b_cksum checksum;
-        /* 96 */ fs::path name;
-        /* 96 + len(name) + 4 */
+        /*  8 */ uint64_t size;
+        /* 16 */ uint64_t archive_offset;
+        /* 24 */ blake2b_cksum checksum;
+        /* 88 */ fs::path name;
+        /* 88 + len(name) + 4 */
 };
 
 void write_u64(uint64_t value) {
@@ -85,15 +84,6 @@ void write_u32(uint32_t value) {
     fwrite(bytes, 1, 4, stdout);
 }
 
-std::pair<uint64_t, uint64_t> get_times(const fs::path & e) {
-    struct statx stat;
-    if (statx(AT_FDCWD, e.c_str(), AT_STATX_SYNC_AS_STAT, STATX_ALL, &stat) == -1) {
-        std::cerr << "statx failed: " << strerror(errno) << std::endl;
-        exit(1);
-    }
-    return std::make_pair(stat.stx_mtime.tv_sec, stat.stx_ctime.tv_sec);
-} 
-
 // Create an archive from directory `dir' and output it to the standard output.
 void create(const char * dir) {
     std::vector<file> files;
@@ -114,9 +104,7 @@ void create(const char * dir) {
         current.size = e.file_size();
 
         // Query the creation/modification times.
-        auto times = get_times(e.path());
-        current.modification_date = std::get<0>(times);
-        current.creation_date = std::get<1>(times);
+        current.modification_date = fs::last_write_time(e.path()).time_since_epoch().count();
 
         // Compute the blake2b checksum.
         current.checksum.from(e.path());
@@ -131,7 +119,7 @@ void create(const char * dir) {
     // Compute the size of metadata.
     uint64_t metadata_size = 0;
     for(auto & f : files)
-        metadata_size += f.name.string().length() + 96 + 4;
+        metadata_size += f.name.string().length() + 88 + 4;
     write_u64(metadata_size);
 
     // Collapse files with the same checksum (assign the same offset).
@@ -151,7 +139,6 @@ void create(const char * dir) {
     // Write the metadata.
     for(auto & f : files) {
         write_u64(f.modification_date);
-        write_u64(f.creation_date);
         write_u64(f.size);
         write_u64(f.archive_offset);
         fwrite(f.checksum.get_digest(), 1, 64, stdout);
@@ -167,8 +154,7 @@ void create(const char * dir) {
             continue;
         }
         assert(f.archive_offset == current_offset);
-        auto times = get_times(base_dir / f.name);
-        if(std::get<0>(times) != f.modification_date) {
+        if(fs::last_write_time(base_dir / f.name).time_since_epoch().count() != f.modification_date) {
             std::cerr << "warning: file " << f.name << " has been modified since the archive was created." << std::endl;
         }
         int fd = open((base_dir / f.name).c_str(), O_RDONLY);
